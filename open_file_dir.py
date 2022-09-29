652e5d55a810ec9f3f2ae4a4358216bca753b555
lnchamb <chamberlain.logan@yahoo.com>
2022-09-29 04:14:52 -0800

    Added a program (hopefully) for opening a folder in the default file explorerer

--- open_file_dir.py
+++ open_file_dir.py
@@ -8,27 +8,34 @@ import sys
 import os
 import subprocess
 
-
-def main(arg):
+def main(argv):
+    arg = argv[1]
     FILE_PATH = os.getcwd()
+    FILE_PATH += "/Program Data/"
+    print(arg)
+    path = os.path.normpath(FILE_PATH)
+    print(path)
+    if os.path.isdir(path):
+        print("ISDIR")
     #Check which file location to open
     if(arg == "Results_FB"):
-        FILE_PATH = "STUB"
+        FILE_PATH += "FoundPosts/FoundPostsFB/"
     if(arg == "Results_IG"):
-        FILE_PATH = "STUB"
+        FILE_PATH += "FoundPosts/FoundPostsIG/"
     if(arg == "Results_TW"):
-        FILE_PATH = "STUB"
+        FILE_PATH += "FoundPosts/FoundPostsTW/"
     if(arg == "Wordlist"):
-        FILE_PATH = "STUB"
+        FILE_PATH += "Wordlists/"
     if(arg == "Flagged_FB"):
-        FILE_PATH = "STUB"
+        FILE_PATH += "FlaggedUsers/FBFlaggedUsers/"
     if(arg == "Flagged_IG"):
-        FILE_PATH = "STUB"
+        FILE_PATH += "FlaggedUsers/IGFlaggedUsers/"
     if(arg == "Flagged_TW"):
-        FILE_PATH = "STUB"
-    
-
-
-
+        FILE_PATH += "FlaggedUsers/TWFlaggedUsers/"
+    PATH = os.path.abspath(FILE_PATH)
+    WIN_FORMAT_PATH = PATH.replace('/', '\\')
+    webbrowser.open(WIN_FORMAT_PATH)
+    os.startfile(WIN_FORMAT_PATH)
+        
 if __name__ == "__main__":
     main(sys.argv)
