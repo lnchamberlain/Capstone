4f2c5447c59c4a8332417775a42fb1c2ab2adea1
lnchamb <chamberlain.logan@yahoo.com>
2022-10-02 20:35:55 -0800

    Added auth.py

---
+++ authenticator.py
@@ -0,0 +1,8 @@
+# UnderCover Recovery 
+# Windows Desktop Application for scraping and storing keywords and flagged users from social media sites 
+# Developed by Tyler and Logan 
+# UAA CSCE Capstone Project Fall 2022
+# Script to post login requests to social media sites, returns cookie values
+#
+
+
