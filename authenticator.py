a59cd2e42d79e01a30208bac3d29c8b26a196a6c
lnchamb <chamberlain.logan@yahoo.com>
2022-10-03 16:14:30 -0800

    added some functionality to authenticator for IG

--- authenticator.py
+++ authenticator.py
@@ -8,6 +8,7 @@ import requests
 import sys
 import datetime
 import json
+import time
 
 class FB_AUTH:
     def __init__(self, username, password):
@@ -28,12 +29,13 @@ class IG_AUTH:
     def attempt_login(self):
         base_url = 'https://www.instagram.com/accounts/login'
         login_url = 'https://www.instagram.com/accounts/login/ajax/'
-
-        current_time = int(datetime.now().timestamp())
+        current_time = time.time()
+        print("Current Time: {}\nUsername: {}\nPassword".format(current_time, self.username, self.password))
         #Have to get csrftoken to put into post header
         initial_response = requests.get(base_url)
-        csrf = response.cookies['csrftoken']
-        
+        print("Inital response is {}".format(initial_response))
+        csrf = initial_response.cookies['csrftoken']
+        print("csrf is {}".format(csrf))
         #build payload
         payload = {
             'username': self.username,
@@ -51,11 +53,20 @@ class IG_AUTH:
         }
 
         login_attempt_response = requests.post(login_url, data=payload, headers=login_header)
-        json_data = json.loads(login_response.text)
+        print("Login attempt response is {}".format(login_attempt_response))
+        json_data = json.loads(login_attempt_response.text)
+        print("\n\n{}\n\n".format(login_attempt_response.text))
 
-        if json_data["authenticated"]:
-            self.cookie = login_response.cookies
-               
+        if "authenticated" in json_data:
+            raw_cookies = login_attempt_response.cookies
+            self.cookie = raw_cookies.get_dict()
+        #Print out successful cookie
+        print("cookie is {}".format(self.cookie))  
+        if(self.cookie == ""):
+            #if unsuccessful, return error message
+            return login_attempt_response.text
+        
+        return "Success"
 
 
 class TW_AUTH:
@@ -70,6 +81,9 @@ def attempt_fb_login(username, password):
    
 def attempt_ig_login(username, password):
     auth = IG_AUTH(username, password)
+    attempt = auth.attempt_login()
+    if(attempt != "Success"):
+        print("Error message: {}".format(attempt))
 
 def attempt_tw_login(username, password):
     auth = TW_AUTH(username, password)
@@ -79,18 +93,19 @@ def attempt_tw_login(username, password):
 if __name__ == "__main__":
     #takes in a mode, username, and password in argv
     #mode can be 31 (FB) 32 (IG) or 33 (TW) 
-    if(len(sys.argv) < 4):
-        #Send an error to the c++ program
-        return 0
+    if(len(sys.argv) > 4):
+        #Send an error to the c++ program 
+        sys.exit()
    
     mode_id = sys.argv[1]
     username = sys.argv[2]
     password = sys.argv[3]
-    if(mode_id == 31):
+    print("Mode ID: {}\nUsername: {}\nPassword: {}".format(mode_id, username, password))
+    if(mode_id == '31'):
         attempt_fb_login(username, password)
-    elif(mode_id == 32):
+    if(mode_id == '32'):
         attempt_ig_login(username, password)
-    elif(mode_id == 33):
+    if(mode_id == '33'):
         attempt_tw_login(username, password)
 
 
