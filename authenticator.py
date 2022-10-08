6e707bc840fa989f844f6aed7f7b88b988d718a1
lnchamb <chamberlain.logan@yahoo.com>
2022-10-08 04:17:53 -0800

    Added scrape_ig.py

--- authenticator.py
+++ authenticator.py
@@ -2,7 +2,7 @@
 # Windows Desktop Application for scraping and storing keywords and flagged users from social media sites 
 # Developed by Tyler and Logan 
 # UAA CSCE Capstone Project Fall 2022
-# Script to post login requests to social media sites, returns cookie values
+# Script to post login requests to social media sites, writes cookie values to log file
 #
 import requests
 import sys
---
+++ scrape_ig.py
@@ -0,0 +1,99 @@
+# UnderCover Recovery 
+# Windows Desktop Application for scraping and storing keywords and flagged users from social media sites 
+# Developed by Tyler and Logan 
+# UAA CSCE Capstone Project Fall 2022
+# Script to scrape Instagram data using region csv selection and keywords list
+# Format for use: python3 scrape_ig.py REGION_SELECTION (int 1-5) OUTPUT FOLDER (CAN PUT DEFAULT)
+# To use with test regions (much smaller): python3 scrape_ig.py 6 DEFAULT
+
+import requests
+import sys
+import datetime
+import json
+import time
+from bs4 import BeautifulSoup
+import csv
+
+
+REGION_RESOLUTION_TABLE = {1:"./Program Data/Regions/ALL_ALASKA.csv", 2:"./Program Data/Regions/ANCHORAGE.csv", 3:"./Program Data/Regions/BETHEL.csv", 4:"./Program Data/Regions/FAIRBANKS.csv", 5:"./Program Data/Regions/JUNEAU.csv", 6:"./Program Data/Regions/TESTING.csv"}
+LOCATION_URLS = []
+KEYWORDS = [] 
+COOKIE = {}
+FLAGGED_POSTS = []
+OUTPUT_DIR = ''
+
+#Fills global variable with urls from the last column of the .csv file
+def get_urls():
+    #Resolve region number into path
+    region_file = REGION_RESOLUTION_TABLE[int(sys.argv[1])]
+    with open(region_file, newline='') as csvfile:
+        csv_data = csv.reader(csvfile, delimiter=',')
+        for row in csv_data:
+            LOCATION_URLS.append(row[-1])
+    #First value is header 'URL'
+    LOCATION_URLS.remove('URL')
+
+
+#Fills global variable with value from wordlist
+def get_keywords():
+    keywords_file = open("./Program Data/Wordlists/keywords.txt", "r+")
+    KEYWORDS = keywords_file.read()
+    
+
+#Fills global variable with value for IG_AUTH logs
+def get_cookie():
+    ig_auth_log_file = open("./Program Data/Logs/IG_AUTH_LOGS/log.txt")
+    #Rebuild cookie dictionary from text file
+    values = ig_auth_log_file.read().split("\n")
+    for line in values:
+        if(line != "SUCCESS"):
+            line = line.split(":")
+            COOKIE[line[0]] = line[1]
+
+
+#Establishes the output directory
+def get_output_dir():
+    dir = sys.argv[2]
+    if(dir == "DEFAULT"):
+        OUTPUT_DIR = "./Program Data/FoundPosts/FoundPostsIG"
+    else:
+        OUTPUT_DIR = dir
+
+
+ #Requests data from location url, formats the return, searches captions and comments for keywords, adds posts to flagged posts
+ def scrape_location(url):
+     print("This is a stub")
+
+
+
+#Requests the full info for a flagged post
+def get_full_info(flagged_post):
+    print("This here is a stub")
+    full_post_info = ''
+    write_output(full_post_info)
+
+
+#Writes output into.html file
+def write_output(post_info):
+    print("And here is another function stub")
+
+
+#Writes full post info to selected output file
+
+def main():
+    get_urls()
+    get_keywords()
+    get_cookie()
+    get_output_dir()
+
+    #GENERAL PROCESS: scrape each location and flag posts, grab full info and write to file
+    for url in LOCATION_URLS:
+        scrape_location(url)
+
+    for post in FLAGGED_POSTS:
+        get_full_info(post)
+    
+
+if __name__ == "__main__":
+    main()
+
