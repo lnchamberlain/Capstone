fc8bb0be523b8824726e3b7766b71f33046cc0f9
lnchamb <chamberlain.logan@yahoo.com>
2022-10-11 15:22:49 -0800

    will put in stuff from our meeting today tonight

--- scrape_ig.py
+++ scrape_ig.py
@@ -3,9 +3,10 @@
 # Developed by Tyler and Logan 
 # UAA CSCE Capstone Project Fall 2022
 # Script to scrape Instagram data using region csv selection and keywords list
-# Format for use: python3 scrape_ig.py REGION_SELECTION (int 1-5) OUTPUT FOLDER (CAN PUT DEFAULT)
+# Format for use: python3 scrape_ig.py REGION_SELECTION (int 1-6) OUTPUT FOLDER (CAN PUT DEFAULT)
 # To use with test regions (much smaller): python3 scrape_ig.py 6 DEFAULT
 
+from xml.sax.xmlreader import Locator
 import requests
 import sys
 import datetime
@@ -17,12 +18,14 @@ import csv
 
 
 REGION_RESOLUTION_TABLE = {1:"./Program Data/Regions/ALL_ALASKA.csv", 2:"./Program Data/Regions/ANCHORAGE.csv", 3:"./Program Data/Regions/BETHEL.csv", 4:"./Program Data/Regions/FAIRBANKS.csv", 5:"./Program Data/Regions/JUNEAU.csv", 6:"./Program Data/Regions/TESTING.csv"}
-LOCATION_URLS = []
+LOCATION_URLS = {}
 KEYWORDS = [] 
 COOKIE = {}
 FLAGGED_POSTS = []
 FLAGGED_USERS = []
 OUTPUT_DIR = ''
+LOCATION_COUNTER = 0
+NUM_LOCATIONS = 0
 
 #Fills global variable with urls from the last column of the .csv file
 def get_urls():
@@ -31,9 +34,9 @@ def get_urls():
     with open(region_file, newline='') as csvfile:
         csv_data = csv.reader(csvfile, delimiter=',')
         for row in csv_data:
-            LOCATION_URLS.append(row[-1])
-    #First value is header 'URL'
-    LOCATION_URLS.remove('URL')
+            LOCATION_URLS[row[1]] = row[-1]
+    #First pair of values are header values
+    LOCATION_URLS.pop("Location Name", None)
 
 
 #Fills global variable with value from wordlist
@@ -67,7 +70,9 @@ def get_flagged_users():
     FLAGGED_USERS = flagged_users_file.read().split(",")
 
  #Requests data from location url, formats the return, searches captions and comments for keywords, adds posts to flagged posts
-def scrape_location(session, url):
+def scrape_location(session, location):
+    print("Scraping {}...".format(location))
+    print("Url is {}".format(LOCATION_URLS[location]))
     response = session.get(url, cookies=COOKIE)
     soup = BeautifulSoup(response.content, 'html.parser')
     #FIGURE OUT HOW TO GRAB THE POSTS HERE 
@@ -97,10 +102,10 @@ def main():
 
     session = requests.Session()
     #GENERAL PROCESS: scrape each location and flag posts, grab full info and write to file
-    for url in LOCATION_URLS:
+    for place in LOCATION_URLS:
         #ADD RANDOM LATENCY HERE
         time.sleep(2)
-        scrape_location(session, url)
+        scrape_location(session, place)
 
     for post in FLAGGED_POSTS:
         #ADD RANDOM LATENCY HERE 
